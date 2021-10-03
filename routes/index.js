bfeb3c436c37e412cbc9b30afa2a922207347caa
Yang <90874019+MARKYangL@users.noreply.github.com>
2021-10-03 11:36:21 +0800

    Completed the routing of the website

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* 1.GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('home', {router: 'home'});
+});
+router.get('/home', function (req, res, next) {
+    res.render('home', {router: 'home'});
+});
+
+
+/* 2.GET about me page. */
+router.get('/about', function (req, res, next) {
+    res.render('about_me', {router: 'about'});
+});
+
+/* 3.GET projects page. */
+router.get('/projects', function (req, res, next) {
+    res.render('projects', {router: 'projects'});
+});
+
+/* 4.GET services page. */
+router.get('/services', function (req, res, next) {
+    res.render('services', {router: 'services'});
+});
+
+
+/* 5.GET contact me page. */
+router.get('/contact', function (req, res, next) {
+    res.render('contact', {router: 'contact'});
+});
+
+module.exports = router;
